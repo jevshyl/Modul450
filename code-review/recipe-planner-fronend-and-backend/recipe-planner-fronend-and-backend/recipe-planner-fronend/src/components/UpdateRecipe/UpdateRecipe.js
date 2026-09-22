1c2a6cb955ea896a152058e5a3d3daf4f4af8bc2
jevshyl <jevgenia.shylovich@edu.tbz.ch>
2026-09-22 14:10:24 +0200

    add task 2

---
+++ code-review/recipe-planner-fronend-and-backend/recipe-planner-fronend-and-backend/recipe-planner-fronend/src/components/UpdateRecipe/UpdateRecipe.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+import './AddRecipe.css';
+import {useForm} from "react-hook-form";
+import {Form, Button, Col, Row, Alert} from 'react-bootstrap';
+import axios from "axios";
+
+const API_BASE_URL = "http://localhost:8080/api";
+
+function UpdateRecipe(props) {
+
+
+
+}
+
+export default UpdateRecipe;
